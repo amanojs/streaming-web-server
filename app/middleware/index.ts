ddf34f427415b24b4841cec97b4b9af97e69d6b8
takashiVue <takashivue@gmail.com>
2021-04-21 14:27:56 +0900

    refactor(tsconfig,package): ビルドの設定をしました
    tsconfigのoutDir,removeCommentsを設定。npm commandにbuildコマンドとビルド済みのアプリケーションを動かすコマンドを追加しました。

--- app/middleware/index.ts
+++ app/middleware/index.ts
@@ -1,13 +1,15 @@
-import {Express} from 'express'
+import { Express } from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
-const bodyParser  = require('body-parser');
+import bodyParser from 'body-parser';
 
 export const setUpMiddleware = (app: Express) => {
-    app.use(helmet({
-        contentSecurityPolicy: false
-    }));
-    app.use(cors());
-    app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(bodyParser.json());
-}
+  app.use(
+    helmet({
+      contentSecurityPolicy: false
+    })
+  );
+  app.use(cors());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+};
