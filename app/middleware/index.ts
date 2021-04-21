df7c125b5942266dfaaa50f0ddaf157dad26a424
amanojs <61931048+amanojs@users.noreply.github.com>
2021-04-21 15:28:06 +0900

    Revert "refactor(tsconfig,package): ビルドの設定をしました"

--- app/middleware/index.ts
+++ app/middleware/index.ts
@@ -1,15 +1,13 @@
-import { Express } from 'express';
+import {Express} from 'express'
 import helmet from 'helmet';
 import cors from 'cors';
-import bodyParser from 'body-parser';
+const bodyParser  = require('body-parser');
 
 export const setUpMiddleware = (app: Express) => {
-  app.use(
-    helmet({
-      contentSecurityPolicy: false
-    })
-  );
-  app.use(cors());
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(bodyParser.json());
-};
+    app.use(helmet({
+        contentSecurityPolicy: false
+    }));
+    app.use(cors());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+}
